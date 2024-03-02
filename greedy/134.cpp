613ac6e5b133e4697ea60b00c376af2ca5ace0ed
fxh <969628251@qq.com>
2024-03-02 14:37:41 +0800

    134.

--- greedy/134.cpp
+++ greedy/134.cpp
@@ -4,24 +4,25 @@
 class Solution {
 public:
     int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost) {
-        int res = 0, sum = 0;
+        int total_sum = 0, cur_sum = 0, start = 0;
         for (int i = 0; i < gas.size(); ++i) {
-            sum = 0;
-            if (gas[i] >= cost[i]) {
-                for (int j = i + 1; ;++j) {
-                    if (j == gas.size()) {
-                        j = 0;
-                        continue;
-                    }
-                    if (j == i) return i;
-                    if (gas[j] >= cost[j]) {
-                        sum = gas[j] - cost[j];
-                        continue;
-                    }
-                }
+            cur_sum += gas[i] - cost[i];
+            total_sum = gas[i] - cost[i];
+            if (cur_sum < 0) {
+                cur_sum = 0;
+                start = i + 1;
             }
-            
         }
-        return -1;
+        if (total_sum < 0) return -1;
+
+        return start;
     }
 };
+
+int main() {
+    std::vector<int> gas{1,2,3,4,5}, cost{3,4,5,1,2};
+    Solution slt;
+    int ans = slt.canCompleteCircuit(gas, cost);
+
+    std::cin.get();
+}
